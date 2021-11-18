dec88d5e26da1cab619874ee96d161b87d5f930d
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-11-19 01:33:46 +0600

    DONE Alhamdulillah

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
         const reviewsCollection = database.collection('reviews');
         const contactUsCollection = database.collection('contactUs');
 
-      
+        // GET all products + Products for home
         app.get('/products', async (req, res) => {
             const place = req.query.place;
             console.log(place);
@@ -47,6 +47,7 @@ async function run() {
             res.json(products);
         })
 
+        // Get All Reviews
         app.get('/reviews', async (req, res) => {
             const cursor = reviewsCollection.find({})
             const products = await cursor.toArray();
@@ -63,17 +64,24 @@ async function run() {
         })
         
         
-    // return orders of a user based on his/her email
-    app.get('/placeOrder', async (req, res) => {
-        console.log(req.headers)
-        console.log(req.headers.authorization)
-        const email = req.query.email;
-        const query = { email: email}
-        console.log(req.decodedEmail, email)
-        const cursor = ordersCollection.find(query);
-        const orders = await cursor.toArray();
-        res.json(orders);    
-        })    
+        // return orders of a user based on his/her email
+        app.get('/myOrders', async (req, res) => {
+            console.log(req.headers)
+            console.log(req.headers.authorization)
+            const email = req.query.email;
+            const query = { email: email}
+            console.log(req.decodedEmail, email)
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.json(orders);    
+        })
+        
+        // GET all orders
+        app.get('/allOrders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.json(orders);    
+            })    
         
         
         // POST Order (Place an order)
@@ -84,8 +92,9 @@ async function run() {
             const result = await ordersCollection.insertOne(orderDetails);
             // console.log(result)
             res.json(result)
-        })    
-
+        })
+        
+        // contact us form
         app.post('/contactUs', async (req, res) => {
             const contactUs = req.body;
             console.log(contactUs)
@@ -94,61 +103,106 @@ async function run() {
             res.json(result)
         })
       
-    // check if the user is an admin
-      app.get('/users/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { email: email };
-        const user = await usersCollection.findOne(query);
-        let isAdmin = false;
-        if (user?.role === 'admin') {
-          isAdmin = true;
-        }
-        res.json({ admin: isAdmin });
-      })
+        // check if the user is an admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+            isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
       
         
-    // for normal registration
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        console.log(result);
-        res.json(result);
-      })
+        // POST / add new feedback by customer
+        app.post('/giveReview', async (req, res) => {
+            const feedback = req.body;
+            // console.log('hit the post api', feedback)
+            const result = await reviewsCollection.insertOne(feedback);
+            // console.log(result);
+            res.json(result);
+        })
+        
+        // POST/add Product
+        app.post('/addProduct', async (req, res) => {
+            const productDetails = req.body;
+            // console.log('hit the post api', feedback)
+            const result = await productsCollection.insertOne(productDetails);
+            // console.log(result);
+            res.json(result);
+        })
+        
+        // for normal registration
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
       
-      // upsert (update or insert) for google sign in
-      app.put('/users', async (req, res) => {
-        const user = req.body;
-        // filter and query almost same
-        const filter = { email: user.email }
-        const options = { upsert: true };
-        const updateDoc = { $set: user };
-        const result = await usersCollection.updateOne(filter, updateDoc, options);
-        res.json(result);
-      })
+        // upsert (update or insert) for google sign in
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            // filter and query almost same
+            const filter = { email: user.email }
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
       
-      // this will only update a existing array, it can't insert a new one (Make Admin)
-    //   app.put('/users/admin', veifyToken, async (req, res) => {
-    //     const user = req.body;
-    //     const requester = req.decodedEmail;
-    //     if (requester) {
-    //       const requesterAccount = await usersCollection.findOne({ email: requester });
-    //       if (requesterAccount.role === 'admin') {
-    //         const filter = { email: user.email };
-    //         const updateDoc = { $set: { role: 'admin' } };
-    //         const result = await usersCollection.updateOne(filter, updateDoc);
-    //         res.json(result);
-    //       } 
-    //     }
-    //     else {
-    //       res.status(403).json({message: "You don't have access to the certain part of the website"});
-    //     }
-    //   })
+        // UPDATE Order Status
+        app.put('/updateStatus/:id', async(req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            console.log(status);
+            // in some doc query is written as filter
+            const query = { _id: ObjectId(id) };
+            // upsert: true means if the updated field doesn't exit it will insert a new one
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    orderStatus: status
+                }
+            }
+            // const update = { orderStatus: status };
+            const result = await ordersCollection.updateOne(query, updateDoc, options)
+            // console.log('load user with id: ', id);
+            res.json(result);
+        })
+        
+        //   this will only update a existing array, it can't insert a new one (Make Admin)
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const requester = req.query.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            console.log(requesterAccount)
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: user.email };
+                const updateDoc = { $set: { role: 'admin' } };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.json(result);
+            }
+            else {
+            res.status(403).json({message: "You don't have access to the certain part of the website"});
+            }
+        })
         
         // DELETE API from MyOrders
         app.delete('/placeOrder/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // DELETE API from Products
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
             res.json(result);
         })
 
